a69c3cdb6cbf21037efbb19ade8a8b2f8aa81749
rahafzaw <zawrahaf@gmail.com>
2025-05-27 12:10:07 -0700

    yala go

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-// import 'order/order_management.dart';
-//
-// import 'search/product_page.dart';
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 // import 'package:firebase_auth/firebase_auth.dart';
-// import 'admin_dash_board_page/admin_dashboard_page.dart';
-// import 'search/search_page.dart';
-// import 'order/order_management.dart';
+
 // import 'package:mobile_app_project/View/search_page_view.dart';
 import 'package:mobile_app_project/View/settings_view.dart';
 import 'package:mobile_app_project/logic/seller_detail_logic.dart';
